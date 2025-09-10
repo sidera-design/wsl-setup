3705d5769f265e822f05929ad1910ab257b3d8e0
Hitoshi Hoshino <hoshino.hitoshi@sidera.jp>
2025-09-10 19:39:59 +0900

    WSL操作コマンドに処理を置き換え

---
+++ Modules/WslUtils/Public/Setup-WslGit.ps1
@@ -0,0 +1,66 @@
+﻿function Setup-WslGit {
+  param(
+    [string]$Distro = $null
+  )
+  # ディストロ名を解決
+  $DistroName = Resolve-WslDistro $Distro 
+  
+  Write-Host ""
+  Write-Host "Gitの設定を行います。" -ForegroundColor Green
+
+  # 定数の設定
+  $GitCredentialManagerPath = "C:\Program Files\Git\mingw64\bin\git-credential-manager.exe"
+
+  # GitがWindowsにインストールされているか確認
+  if (-not (Get-Command git.exe -ErrorAction SilentlyContinue)) {
+      Write-Warning "Windows上にGitがインストールされていません。"
+      Write-Host "インストールしてから再度このスクリプトを実行してください。" -ForegroundColor Cyan
+      Read-Host "[Enter]でGitのダウンロードページを表示します。"
+      Start-Process "https://git-scm.com/downloads"
+      Write-Host ""
+      exit 1
+  }
+
+  # Git Credential Managerのパスを確認
+  if (-not (Test-Path $GitCredentialManagerPath)) {
+      Write-Error "Git Credential Managerが見つかりません。"
+      Write-Host "Git Credential Managerが以下のパスに正しくインストールされているか確認してください。" -ForegroundColor Cyan
+      Write-Host "  $GitCredentialManagerPath"
+      Write-Host "【参考】Gitバージョンの差異による可能性があります。" -ForegroundColor DarkGray
+      Write-Host "  v2.36.1～v2.38.x : mingw64\libexec\git-core\git-credential-manager.exe" -ForegroundColor DarkGray
+      Write-Host "  v2.36.0以前 : mingw64\bin\git-credential-manager-core.exe” -ForegroundColor DarkGray
+      Write-Host ""
+      exit 1
+  }
+  
+
+  # WSL上でgitのバージョンを表示
+  Write-Host "Gitバージョンを確認します。" -ForegroundColor Cyan
+  $winGitVersion = git --version
+  $wslGitVersion = wsl -d $DistroName -- git --version
+  Write-Host " Windows: $winGitVersion"
+  Write-Host " WSL: $wslGitVersion"
+
+  Write-Host "WSL上のgit config を設定します。" -ForegroundColor Cyan
+  # Gitユーザー名を設定
+  $gitUserName = git config --global user.name
+  if (-not $gitUserName) {
+      Write-Warning "Gitにユーザー名が設定されていません。設定します。"
+      $gitUserName = Read-Host "Git用ユーザー名を入力してください"
+      git config --global user.name $gitUserName
+  }
+  Write-Host " Gitユーザー名: $gitUserName"
+  wsl -d $DistroName -- bash -lc "git config --global user.name '$gitUserName'"
+  # Gitメールアドレスを設定
+  $gitUserEmail = git config --global user.email
+  if (-not $gitUserEmail) {
+      Write-Warning "Gitにメールアドレスが設定されていません。設定します。"
+      $gitUserEmail = Read-Host "Git用メールアドレスを入力してください"
+      git config --global user.email $gitUserEmail
+  }
+  Write-Host " Gitメールアドレス: $gitUserEmail"
+  wsl -d $DistroName -- bash -lc "git config --global user.email '$gitUserEmail'"
+  # Git Credential Managerの設定
+  $GcmPathWSL = Convert-PathToWsl $GitCredentialManagerPath -Distro $DistroName
+  wsl -d $DistroName -- bash -lc "git config --global credential.helper '$GcmPathWSL'"
+}
--- install.ps1
+++ install.ps1
@@ -1,24 +1,26 @@
-﻿# 定数設定
-$GitSetupFileName = "setup_git.ps1"
+﻿Import-Module '.\Modules\WslUtils\WslUtils.psd1' -Force *> $null
+
+# 定数設定
 $WslSetupFileName = "setup.sh"
 $DefaultDistroName = "Ubuntu-Sample"
 
+# 引数でディストロ名が指定されていなければ既定のディストロ名を使う
 if ($args.Count -gt 0) {
     $WslDistroName = $args[0]
-} else {
+}
+else {
     $WslDistroName = $DefaultDistroName
 }
 
 # WSLの設定を確認
 if (-not (Get-Command wsl -ErrorAction SilentlyContinue)) {
-    Write-Warning "WSL機能がインストールされていません。WSLを有効にしてから再度実行してください。"
+    Write-Warning "WSL機能がインストールされていません。WSLを有効する必要があります。"
+    Write-Host "先に .\setup_win.ps1 を実行してください。"
     exit 1
 }
 
 # WSLでインストール済みディストリビューションを取得
-$distros = wsl --list --quiet 2>$null
-
-if (-not ($distros -contains "$WslDistroName") ){
+if (-not (Test-WslDistroExists $WslDistroName) ) {
     Write-Host "WSLに '$WslDistroName' がインストールされていません。" 
     try {
         Write-Host "Ubuntu ($WslDistroName) をインストールします。" -ForegroundColor Green
@@ -46,51 +48,12 @@ else {
 }
 
 # Gitの設定
-$setupGitScript = Join-Path -Path (Split-Path -Parent $MyInvocation.MyCommand.Path) -ChildPath "$GitSetupFileName"
-if (Test-Path $setupGitScript) {  
-    & $setupGitScript
-    if ($LASTEXITCODE -ne 0) {
-        exit 1
-    }
-} else {
-    Write-Error "$GitSetupFileName が見つかりません。"
-    exit 1
-}
-
+Setup-WslGit -Distro $WslDistroName
 
 # $WslSetupFileName を WSL の /tmp ディレクトリにコピーして実行
 $setupShPath = Join-Path -Path (Split-Path -Parent $MyInvocation.MyCommand.Path) -ChildPath "$WslSetupFileName"
-if (Test-Path $setupShPath) {
-    $setupShWslPath = wsl -d $WslDistroName -- wslpath -u "'$setupShPath'"
-    $setupShHomePath = "/tmp/$WslSetupFileName"
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "$WslSetupFileName のパス変換に失敗しました。"
-        exit 1
-    }
-    # /tmp ディレクトリにコピー
-    wsl -d $WslDistroName -- cp "$setupShWslPath" "$setupShHomePath"
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "$setupShWslPath から $setupShHomePath へのコピーに失敗しました。"
-        exit 1
-    }
-    # 改行コードをLinux形式に変換
-    wsl -d $WslDistroName -- sed -i "'s/\r$//'" "$setupShHomePath"
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "$setupShHomePath の改行コードの変換失敗しました。"
-        exit 1
-    }
-    # $WslSetupFileName 実行
-    Write-Host ""
-    Write-Host "WSL上で設定スクリプト($WslSetupFileName)を実行します。" -ForegroundColor Green
-    wsl -d $WslDistroName -- bash "$setupShHomePath"
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "$setupShHomePath の実行に失敗しました。"
-        exit 1
-    }
-} else {
-    Write-Error "$WslSetupFileName が見つかりません。"
-    exit 1
-}
+Invoke-WslScript $setupShPath -Distro $WslDistroName
+
 
 # WSL設定の終了
 Write-Host ""
